e7c8da75c974621279b850f6d7764e58c4b0ccae
Tadeo Lopez <lrstadeo@hotmail.com>
2025-11-24 13:00:07 -0300

    arreglando editar

--- js/app.js
+++ js/app.js
@@ -8,14 +8,24 @@
         if(window.indexedDB.open('crm', 1)) {
             obtenerClientes();
         }
+        listadoClientes.addEventListener('click', editarRegistros)
         listadoClientes.addEventListener('click', eliminarRegistro);
     });
     
+    function editarRegistros(e) {
+    e.preventDefault()
+    if (e.target.classList.contains('editar')) {
+      const id = Number(e.target.dataset.cliente)
+ 
+      location.href = `editar-cliente.html?id=${id}`
+    }
+    }
+
+
     function eliminarRegistro(e) {
         e.preventDefault();
 
         if(e.target.classList.contains('eliminar')) {
-            console.log('first')
             const idEliminar = Number(e.target.dataset.cliente);
             const confirmar = confirm('¿Deseas eliminar este cliente?');
 
@@ -104,25 +114,27 @@
                     const { nombre, empresa, email, telefono, id } = cursor.value;
                     
                     const listadoClientes = document.querySelector('#listado-clientes');
-                    listadoClientes.innerHTML +=
-                    `<tr>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
-                            <p class="text-sm leading-10 text-gray-700"> ${email} </p>
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
-                            <p class="text-gray-700">${telefono}</p>
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
-                            <p class="text-gray-600">${empresa}</p>
-                        </td>
-                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
-                            <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
-                        </td>
-                    </tr>`;
+                    listadoClientes.innerHTML += `
+
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${telefono}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${empresa}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="#" data-cliente="${id}" class="text-teal-600 hover:text-teal-900 mr-5 editar">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                            </td>
+                        </tr>
+                    `;
         
-                    cursor.continue();
+                    cursor.continue()
                  } 
              };
 
